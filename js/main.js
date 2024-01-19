cf9b4742a5ce67a881f2c8530c0e2d0af925d029
hgkim <ljm5704@gmail.com>
2024-01-19 17:13:20 +0900

    1차 인터렉션 추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function(){
-
  /* header */
   function ham_btn(_target){
     var _this = document.querySelector(_target)
@@ -13,10 +12,44 @@ $(function(){
   }
   ham_btn('.hamber_btn')
   $('.hamber_btn').on('click', function(){
+    $('html, body').toggleClass('overflow');
     $('nav').toggleClass('show');
   });
 
+
+  
+  function motion(){
+    var scrt = $(window).scrollTop()
+    var winh = $(window).height()
+    var t_1
+    var t_2
+    $('.bind').each(function(){
+      t_1 = $(this).offset().top
+      if(scrt >= t_1 - winh){
+        $(this).children('img').addClass('view_lt')
+        $(this).children('ul').addClass('view_rt')
+      }
+    })//each
+    $('.goods').each(function(){
+      t_2 = $(this).offset().top
+      if(scrt >= t_2 * 0.5){
+        $(this).children('.inner').children('dl').addClass('view_bottom')
+      }
+    })
+    console.log(t_2 + '/' + scrt + '/' + winh);
+  }//fn
+
+  $(window).scroll(function(){
+    motion()
+  })
+
 /* main */
+
+
+
+
+
+
 
 
 })//ready
