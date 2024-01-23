d148d7f3096c3cd4e88f12ec2d3147ffcf0100a6
hgkim <ljm5704@gmail.com>
2024-01-23 11:32:52 +0900

    pro 완성

--- js/pro.js
+++ js/pro.js
@@ -11,12 +11,13 @@ function fadein(){
   var scrt = $(window).scrollTop()
   var t
   t = $('.info').offset().top
+  console.log(scrt)
   console.log(t)
-  if(scrt >= t * 0.5){
+  if(scrt >= t*0.8){
     var info_dl = $('.info').children('dl')
     info_dl.each(function(i){
       info_dl.eq(i).addClass('fadein')
-      info_dl.eq(i).css({"transition-delay":0.1*i+'s'})
+      info_dl.eq(i).css({"transition-delay": (0.5*i)+'s'})
     });
   };
 }
